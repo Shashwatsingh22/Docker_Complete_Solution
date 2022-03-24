70e896099c29c28b9a69986e95845ba7b798b65e
Shashwat Singh (alias Ma4akaa1) <shashwatsingh71@gmail.com>
2022-03-24 21:27:13 +0530

    app init

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+//Routes
+const contSer = require('./api/container')
+const imgSer = require('./api/img')
+const netWorkSer = require('./api/network')
+const dockerFileSer =  require('./api/dockerFile');
+const res = require('express/lib/response');
+
+//For Logs Managment 
+app.use(morgan('dev'))
+
+//For Parsing Body
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+//Resolving Cors Error 
+app.use((req,res,next)=>{
+    //Here, we set the Header Mannually
+    res.header('Access-Control-Allow-Origin','*') //Here, We can specify the Specific endpoint
+    res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorixation');
+
+    if(req.method==='OPTIONS')
+    {
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+})
+
+//Root
+app.use('/',(req,res,next)=>{
+    res.status(200).json({
+        msg : "Docker API service Started Bose 😎"
+    })
+})
+
+//Container Service
+app.use('/contSer', contSer);
+
+//Image Service
+app.use('/imgSer',imgSer);
+
+//Network Service
+app.use('/netSer',netWorkSer);
+
+//DockerFile Serivces
+app.use('/dockFileSer',dockerFileSer);
+
+//If Some Error Caused or Wrong Routes
+app.use((req,res,next)=>
+{
+    const error=new Error("Not Found 😥");
+    error.status=404;
+    next(error);
+})
+
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500);
+    res.json({
+      error:{
+          message: error.message
+      }  
+    })
+})
+
+module.exports=app;
---
+++ server.js
@@ -0,0 +1,11 @@
+//Predefined Library
+const http = require('http');
+
+//Our Custom Files
+const app=require('./app')
+
+const port =  process.env.PORT || 3000;
+
+const server=http.createServer(app);
+
+server.listen(port);
