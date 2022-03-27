244fb423125d77d24052fd3235dd1f9f3b965098
Shashwat Singh (alias Ma4akaa1) <shashwatsingh71@gmail.com>
2022-03-28 01:05:10 +0530

    _

--- app.js
+++ app.js
@@ -7,25 +7,19 @@ var path = require('path');
 var nodemailer = require('nodemailer');
 var objectId = require('mongodb').ObjectID;
 var assert = require('assert');
+
 const bcrypt = require('bcrypt');
 const app = express()
 const User = require('./models/user');
+//const Machine = require('./models/machine')
 const { render } = require("ejs");
+
 const axios = require('axios');
 const user = require("./models/user");
-const loginS = require("./routes/login");
-const optionS = require("./routes/option");
-const signupS = require("./routes/sign_up");
-const controlS = require("./routes/control");
-const serverlessS = require("./routes/serverless");
-const pullS = require("./routes/pull");
-const imgser = require("./routes/img-ser");
-const contser = require("./routes/cont-ser");
-const netser = require("./routes/net-ser");
-const imgbuildser = require("./routes/image_build");
-const volser = require("./routes/vol_ser");
-const configureS = require("./routes/configure");
-const contoptionsS = require("./routes/contoptions");
+
+
+
+
 app.use(bodyParser.json())
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
@@ -38,6 +32,7 @@ mongoose.connect('mongodb+srv://aditya_admin:'+ process.env.MONGO_DB_PASS + '@cl
     useUnifiedTopology: true
 });
 var db = mongoose.connection;
+
 var mip;
 var userN;
 var wdata="";
@@ -50,20 +45,748 @@ app.get("/",(req,res)=>{
     })
     return res.redirect('./pages/index.html');
 }).listen(8080);
-app.use(configureS);
-app.use(loginS);
-app.use(signupS)
-app.use(optionS)
-app.use(controlS);
-app.use(pullS);
-app.use(serverlessS)
-app.use(imgser);
-app.use(contser);
-app.use(imgbuildser);
-app.use(volser);
-app.use(netser);
-app.use(contoptionsS)
+
+
+app.post("/configure", (req, res, next) => {
+    User.find({ username: req.body.username })
+      .exec()
+      .then(user => {
+        if (user.length >= 1) {
+          console.log('Username exists');
+        } else {
+          bcrypt.hash(req.body.password, 10, (err, hash) => {
+            if (err) {
+              console.log('Error')
+            } else {
+              const user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                //machineip: req.body.machineip,
+                username: req.body.username,
+                password: hash
+              });
+              user
+              .save()
+              .then(result => {
+                console.log(result);
+                
+              })
+              .catch(err => {
+                console.log(err);
+              })
+              if(req.body.email==="admin@gmail.com"){
+                res.redirect('data.ejs')
+              }
+              else{      
+                User.find({email: req.body.email})
+                .select(" email ")
+                .exec()
+                .then(docs => {
+                  const response = {
+                    
+                    users: docs.map(doc => {
+                      return {
+                        
+                        email: doc.email
+                        
+                      };
+                    })
+                  };
+                  userN=req.body.email;
+                  res.render('option', { title: 'Developer Records', user: userN,})
+                })
+              
+        }
+     
+            }
+    // db.collection('users').insertOne(user,(err,collection)=>{
+    //     if(err){
+    //         throw err;
+    //     }
+    //     console.log("Record Inserted Successfully");
+    // });
+
+    // return res.redirect('signup_success.html');
+});
+}});
+});
+
+app.post("/sign_up", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length >= 1) {
+        console.log('Mail exists');
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            console.log('Error')
+          } else {
+            const user = new User({
+              _id: new mongoose.Types.ObjectId(),
+              username: req.body.username,
+              email: req.body.email,
+              password: hash
+            });
+            user
+            .save()
+            .then(result => {
+              console.log(result);
+            })
+            .catch(err => {
+              console.log(err);
+            })
+            userN=req.body.username;
+            res.render('option', { title: 'Developer Records', user: userN})
+            
+      }
+   
+      
+  // db.collection('users').insertOne(user,(err,collection)=>{
+  //     if(err){
+  //         throw err;
+  //     }
+  //     console.log("Record Inserted Successfully");
+  // });
+
+  // return res.redirect('signup_success.html');
+});
+}});
+});
+app.get("/login", (req, res) => {
+  res.set({
+      "Allow-access-Allow-Origin": '*'
+  })
+  return res.redirect('/pages/login.html');
+})
+app.get("/sign_up", (req, res) => {
+  res.set({
+      "Allow-access-Allow-Origin": '*'
+  })
+  return res.redirect('/pages/signup.html');
+})
+app.get("/signup_success", (req, res) => {
+  res.set({
+      "Allow-access-Allow-Origin": '*'
+  })
+  return res.redirect('/pages/signupsuccess.html');
+})
+app.get("/option", (req, res) => {
+  res.set({
+      "Allow-access-Allow-Origin": '*'
+  })
+  return res.redirect('option.ejs');
+})
+app.get("/control", (req, res) => {
+  res.set({
+      "Allow-access-Allow-Origin": '*'
+  })
+  res.render('control.ejs');
+})
+app.get("/owned", (req, res) => {
+  res.set({
+      "Allow-access-Allow-Origin": '*'
+  })
+  return res.redirect('owned.ejs');
+})
+app.get("/pull", (req, res) => {
+  res.set({
+      "Allow-access-Allow-Origin": '*'
+  })
+  res.render('pull.ejs',{mip: mip, userN: userN});
+})
+app.get("/serverless", (req, res) => {
+  res.set({
+      "Allow-access-Allow-Origin": '*'
+  })
+  res.render('serverless.ejs');
+})
+app.post("/serverless", (req, res) => {
+  res.render('serverless.ejs',{});
+})
+
+app.get("/img-ser", (req, res) => {
+  res.set({
+      "Allow-access-Allow-Origin": '*'
+  })
+  return res.render('img.ejs',{
+    mip : mip,
+    userN : userN,
+    data : wdata
+  });
+})
+
+
+app.get("/cont-ser", (req, res) => {
+  res.set({
+      "Allow-access-Allow-Origin": '*'
+  })
+  return res.render('cont.ejs',{
+    mip : mip,
+    userN : userN,
+    data : wdata
+  });
+})
+
+app.get("/contoptions", (req, res) => {
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.render('contoptions.ejs',{
+      mip : mip,
+      userN : userN,
+      data : wdata
+    });
+  })
+
+app.get("/img-build-ser", (req, res) => {
+  res.set({
+      "Allow-access-Allow-Origin": '*'
+  })
+  return res.render('image_build.ejs',{
+    mip : mip,
+    userN : userN,
+    data : wdata
+  });
+})
+
+app.get("/vol-ser", (req, res) => {
+  res.set({
+      "Allow-access-Allow-Origin": '*'
+  })
+  return res.render('vol_ser.ejs',{
+    mip : mip,
+    userN : userN,
+    data : wdata
+  });
+})
+
+app.get("/net-ser", (req, res) => {
+  res.set({
+      "Allow-access-Allow-Origin": '*'
+  })
+  return res.render('net.ejs',{
+    mip : mip,
+    userN : userN,
+    data: wdata
+  });
+})
+
+app.post("/login", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length < 1) {
+        console.log('Auth Failed')
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          console.log('Auth Failed');
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id
+            },
+            "" +process.env.JWT_KEY,
+            {
+                expiresIn: "1h"
+            }
+          );
+        
+            console.log('Auth successful');
+            console.log(token);
+            userN=req.body.username;
+            res.render('option', { title: 'Developer Records', user: userN})
+        }
+        
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+    
+});
+
+
+app.post("/pull", (req, res, next) => {
+  mip=req.body.machineip;
+  console.log("from the main ser ",mip)  
+  res.render('pull.ejs', { 
+    mip: mip,
+    userN : userN,
+   })
+});
+
+// app.post("/owned.ejs", (req, res, next) => {
+//   User.find({}).select("machineip").exec(function(err, data){
+//     if(err) throw err;
+//     res.render('owned', { title: 'User Records', records: data })
+//   })
+// });
+
+app.post("/pull", (req, res, next) => {
+  User.find({}).select("machineip").exec(function(err, data){
+    if(err) throw err;
+    res.render('pull', { title: 'User Records', records: data })
+  })
+})
+// app.post("/img-ser", (req, res) => {
+//   const dim = req.body.dockerimagename;
+//   const duser = req.body.dockerusername;
+//   const dpass = req.body.dockerpassword;
+//   const dver = req.body.dockerversion;
+//   console.log(dim);
+//   console.log(duser);
+//   console.log(dpass);
+//   console.log(dver);
+// })
+
+ 
+
 //--> Image Service API's <----
+
+app.post("/img-ser",(req,res)=>
+{
+   var subSer= req.body.subSer;
+   
+   var apiUrl = "http://"+mip+":3000/imgSer";
+   
+   if(subSer  === "list")
+   {
+    apiUrl += '/view';
+        
+        console.log(apiUrl);
+
+         
+          axios.get(apiUrl,{
+            data:{
+              'action' : 'list'
+          }})
+          .then((ress) => {
+              console.log(`Status: ${ress.status}`);
+              console.log('Body: ', ress.data);
+
+              wdata=ress.data['output'];
+              res.render('img', {userN: userN, mip : mip, data : wdata })
+
+          }).catch((err) => {
+              console.error(err);
+          });
+
+        // const run = async()=>{
+        //     try {
+        //       const res = await axios.get(apiUrl,data);
+        //       console.log(`status: ${res.status}`);
+        //       console.log('Body : ', res.data);
+        //     }catch(err){
+        //       console.log(err);
+        //     }
+        //   }
+
+         //run();
+        // axios.get(apiUrl).then(
+        //   response => 
+        //   {
+        //     wdata = response.data;
+        //     console.log(wdata);
+            
+          //  res.render('img', {userN: userN, mip : mip, data : wdata })
+        //   }
+        // ).catch(
+        //   err=>{
+        //     console.log("Error in Fetching the Data from the API")
+        //   }
+        // ) 
+   }
+
+   else if(subSer === "pull")
+   {
+    var imgName = req.body.dockerimagename;
+    
+    apiUrl+='/change';
+
+        
+    console.log(apiUrl);
+     
+    axios.get(apiUrl,{
+        data:{
+          'action' : 'pull',
+          'name_with_tag' : imgName
+      }})
+      .then((ress) => {
+          console.log(`Status: ${ress.status}`);
+          console.log('Body: ', ress.data);
+          wdata=ress.data['output'];
+          res.render('img', {userN: userN, mip : mip, data : wdata })
+
+
+      }).catch((err) => {
+          console.error(err);
+      });
+    // axios.get(apiUrl).then(
+    //   response => 
+    //   {
+    //     wdata = response.data;
+    //     console.log(wdata);
+        
+    //     res.render('img', {userN: userN, mip : mip, data : wdata })
+    //   }
+    // ).catch(
+    //   err=>{
+    //     console.log("Error in Fetching the Data from the API")
+    //   }
+    // ) 
+   }
+
+   else if(subSer === "rmbyid")
+   {
+    var imgid = req.body.imgid;
+    apiUrl=apiUrl+"rm_img_by_id&img_id="+imgid;
+
+        
+    console.log(apiUrl);
+
+    axios.get(apiUrl).then(
+      response => 
+      {
+        wdata = response.data;
+        console.log(wdata);
+        
+        res.render('img', {userN: userN, mip : mip, data : wdata })
+      }
+    ).catch(
+      err=>{
+        console.log("Error in Fetching the Data from the API")
+      }
+    ) 
+   }
+
+   else if(subSer === "hist")
+   {
+    var imgid = req.body.imgid;
+    apiUrl=apiUrl+"img_hist&img_id="+imgid;
+
+        
+    console.log(apiUrl);
+
+    axios.get(apiUrl).then(
+      response => 
+      {
+        wdata = response.data;
+        console.log(wdata);
+        
+        res.render('img', {userN: userN, mip : mip, data : wdata })
+      }
+    ).catch(
+      err=>{
+        console.log("Error in Fetching the Data from the API")
+      }
+    )
+   }
+
+   else if(subSer === "insp")
+   {
+    var imgid = req.body.imgid;
+    apiUrl=apiUrl+"img_inspect&img_id="+imgid;
+
+        
+    console.log(apiUrl);
+
+    axios.get(apiUrl).then(
+      response => 
+      {
+        wdata = response.data;
+        console.log(wdata);
+        
+        res.render('img', {userN: userN, mip : mip, data : wdata })
+      }
+    ).catch(
+      err=>{
+        console.log("Error in Fetching the Data from the API")
+      }
+    )
+   }
+})
+
 // Container API service
+
+app.post("/cont-ser",(req,res)=>
+{
+   var subSer= req.body.subSer;
+   var apiUrl = "http://"+mip+":3000/contSer";
+
+   if(subSer  === "list")
+   {
+        apiUrl=apiUrl+"ls"
+        
+        console.log(apiUrl);
+
+        axios.get(apiUrl).then(
+          response => 
+          {
+            wdata = response.data;
+            console.log(wdata);
+            
+            res.render('cont', {userN: userN, mip : mip, data : wdata })
+          }
+        ).catch(
+          err=>{
+            console.log("Error in Fetching the Data from the API")
+          }
+        ) 
+   }
+
+   else if(subSer === "insp")
+   {
+     var contName=req.body.contName;
+    apiUrl=apiUrl+"inspect_by_name&cont_name="+contName;
+        
+    console.log(apiUrl);
+
+    axios.get(apiUrl).then(
+      response => 
+      {
+        wdata = response.data;
+        console.log(wdata);
+        
+        res.render('cont', {userN: userN, mip : mip, data : wdata })
+      }
+    ).catch(
+      err=>{
+        console.log("Error in Fetching the Data from the API")
+      }
+    ) 
+   }
+
+   else if(subSer === "deploy")
+   {
+     var inPort=req.body.in_Port;
+     var outPort=req.body.out_Port;
+     var imgName = req.body.image_name_with_ver;
+     var tag = req.body.tag;
+
+     
+
+    //apiUrl=apiUrl+"deploy&in_port="+inPort+"&out_port="+outPort+"&img_name_with_tag="+imgName+"&contName="+contName;
+    apiUrl+='/change';
+
+    console.log(apiUrl);
+    
+    axios.get(apiUrl,{
+        data:{
+          'action' : 'deploy',
+          'in_port' : inPort,
+          'out_port' : outPort,
+          'img_name_with_ver' : imgName,
+          'tag' : tag
+      }})
+      .then((ress) => {
+          console.log(`Status: ${ress.status}`);
+          console.log('Body: ', ress.data);
+          wdata=ress.data['output'];
+          res.render('img', {userN: userN, mip : mip, data : wdata })
+
+
+      }).catch((err) => {
+          console.error(err);
+      }); 
+
+    // axios.get(apiUrl).then(
+    //   response => 
+    //   {
+    //     wdata = response.data;
+    //     console.log(wdata);
+        
+    //     res.render('cont', {userN: userN, mip : mip, data : wdata })
+    //   }
+    // ).catch(
+    //   err=>{
+    //     console.log("Error in Fetching the Data from the API")
+    //   }
+    // ) 
+   }
+
+   else if(subSer === "stop")
+   {
+    
+     var contName = req.body.contName;
+
+     apiUrl=apiUrl+"stop&cont_name="+contName;
+        
+    console.log(apiUrl);
+
+    axios.get(apiUrl).then(
+      response => 
+      {
+        wdata = response.data;
+        console.log(wdata);
+        
+        res.render('cont', {userN: userN, mip : mip, data : wdata })
+      }
+    ).catch(
+      err=>{
+        console.log("Error in Fetching the Data from the API")
+      }
+    ) 
+   }
+
+   else if(subSer === "start")
+   {
+     var contName = req.body.contName;
+
+    apiUrl=apiUrl+"start&cont_name="+contName;
+        
+    console.log(apiUrl);
+
+    axios.get(apiUrl).then(
+      response => 
+      {
+        wdata = response.data;
+        console.log(wdata);
+        
+        res.render('cont', {userN: userN, mip : mip, data : wdata })
+      }
+    ).catch(
+      err=>{
+        console.log("Error in Fetching the Data from the API")
+      }
+    ) 
+   }
+
+   else if(subSer === "log")
+   {
+     var contName = req.body.contName;
+    apiUrl=apiUrl+"log&cont_name="+contName;
+        
+    console.log(apiUrl);
+
+    axios.get(apiUrl).then(
+      response => 
+      {
+        wdata = response.data;
+        console.log(wdata);
+        
+        res.render('cont', {userN: userN, mip : mip, data : wdata })
+      }
+    ).catch(
+      err=>{
+        console.log("Error in Fetching the Data from the API")
+      }
+    ) 
+   }
+
+
+  })
+
+
   //-- Network Service ---
+
+  app.post("/net-ser",(req,res)=>
+{
+   var subSer= req.body.subSer;
+   var apiUrl = "http://"+mip+"/cgi-enabled/index.py?service=net&subser=";
+   
+   if(subSer  === "ls")
+   {
+        apiUrl=apiUrl+"ls"
+        
+        console.log(apiUrl);
+
+        axios.get(apiUrl).then(
+          response => 
+          {
+            wdata = response.data;
+            console.log(wdata);
+            
+            res.render('net', {userN: userN, mip : mip, data : wdata })
+          }
+        ).catch(
+          err=>{
+            console.log("Error in Fetching the Data from the API")
+          }
+        ) 
+   }
+
+   else if(subSer  === "create")
+   {
+      var subnet = req.body.subnet;
+      var net_name = req.body.netName; 
+
+        apiUrl=apiUrl+"create&subnet="+subnet+"&net_name="+net_name;
+        
+        console.log(apiUrl);
+
+        axios.get(apiUrl).then(
+          response => 
+          {
+            wdata = response.data;
+            console.log(wdata);
+            
+            res.render('net', {userN: userN, mip : mip, data : wdata })
+          }
+        ).catch(
+          err=>{
+            console.log("Error in Fetching the Data from the API")
+          }
+        ) 
+   }
+
+   else if(subSer  === "deploy")
+   {
+      var ip = req.body.ip;
+      var netName =  req.body.netName;
+      var inPort = req.body.inPort;
+      var outPort = req.body.outPort;
+      var contName = req.body.contName;
+      var imgNameWithTag = req.body.imgNameWithTag;
+
+        apiUrl=apiUrl+"deploy_with_static&ip="+ip+"&net_name="+netName+"&in_port="+inPort+"&out_port="+outPort+"&cont_name="+contName+"&img_name_with_tag="+imgNameWithTag;
+        
+        console.log(apiUrl);
+
+        axios.get(apiUrl).then(
+          response => 
+          {
+            wdata = response.data;
+            console.log(wdata);
+            
+            res.render('net', {userN: userN, mip : mip, data : wdata })
+          }
+        ).catch(
+          err=>{
+            console.log("Error in Fetching the Data from the API")
+          }
+        ) 
+   }
+
+   else if(subSer  === "capIP")
+   {
+      var contName = req.body.contName;
+      
+      apiUrl=apiUrl+"cap_ip&cont_name="+contName;
+        
+        console.log(apiUrl);
+
+        axios.get(apiUrl).then(
+          response => 
+          {
+            wdata = response.data;
+            console.log(wdata);
+            
+            res.render('net', {userN: userN, mip : mip, data : wdata })
+          }
+        ).catch(
+          err=>{
+            console.log("Error in Fetching the Data from the API")
+          }
+        ) 
+   }
+   
+   
+  })
+
 console.log("Listening on PORT 8080");
--- routes/img-ser.js
+++ routes/img-ser.js
@@ -1,3 +1,4 @@
+const { default: axios } = require("axios");
 var express = require("express");
 var routes = express.Router();
 var mip;
@@ -17,27 +18,42 @@ routes.get("/img-ser", (req, res) => {
 routes.post("/img-ser",(req,res)=>
 {
    var subSer= req.body.subSer;
-   var apiUrl = "http://"+mip+"/cgi-enabled/index.py?service=img&subser=";
+    console.log(mip);
+   var apiUrl = "http://"+mip+":3000";
    
    if(subSer  === "list")
    {
-        apiUrl=apiUrl+"list"
+        apiUrl += '/imgSer/view';
+        
+        const data = {
+          action : 'list'
+        }
         
         console.log(apiUrl);
 
-        axios.get(apiUrl).then(
-          response => 
-          {
-            wdata = response.data;
-            console.log(wdata);
+        const run = async()=>{
+          try {
+            const res = await axios.get(apiUrl,data);
+            console.log(`status: ${res.status}`);
+            console.log('Body : ', res.data);
+          }catch(err){
+            console.log(err);
+          }
+        }
+
+        // axios.get(apiUrl).then(
+        //   response => 
+        //   {
+        //     wdata = response.data;
+        //     console.log(wdata);
             
             res.render('img', {userN: userN, mip : mip, data : wdata })
-          }
-        ).catch(
-          err=>{
-            console.log("Error in Fetching the Data from the API")
-          }
-        ) 
+        //   }
+        // ).catch(
+        //   err=>{
+        //     console.log("Error in Fetching the Data from the API")
+        //   }
+        // ) 
    }
 
    else if(subSer === "pull")
